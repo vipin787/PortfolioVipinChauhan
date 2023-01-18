e05cc63ee04e2c6d5b9a9231239bd473f8e0f774
vipin <cvipin787@gmail.com>
2023-01-18 13:00:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// // console.log("running...") 
+
+ document.querySelector('.cross').style.display = 'none'; 
+ document.querySelector('.hamburger').addEventListener("click", () => {
+     document.querySelector('.sidebar').classList.toggle('.sidebarGO');
+});
